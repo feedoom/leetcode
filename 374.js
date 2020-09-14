b328436705531cfc4b0239b36522e24dcbe8dcec
feedoom <qqdz3750@sina.com>
2020-09-14 17:03:45 +0800

    分而治之解决 374

--- 374.js
+++ 374.js
@@ -13,3 +13,19 @@ var guessNumber = function(n) {
     }
   }
 };
+
+var guessNumber = function(n) {
+  const rec = (low, high) => {
+    if (low > high) return ;
+    const mid = Math.floor((low + high) / 2)
+    const flag = guess(mid)
+    if (flag == 1) {
+      return rec(mid + 1, high)
+    } else if (flag == -1) {
+      return rec(1, mid - 1)
+    } else {
+      return mid
+    }
+  }
+  return rec(1, n)
+}
