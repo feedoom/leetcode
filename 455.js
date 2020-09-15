e3cabdbb1ee2051d9119013f39cabeae0cf2f9c7
feedoom <qqdz3750@sina.com>
2020-09-15 10:43:00 +0800

    finish 455 -> 贪心算法分饼干

---
+++ 455.js
@@ -0,0 +1,12 @@
+var findContentChildren = function(g, s) {
+  const sortFunc = function(a, b) {
+    return a - b
+  }
+  g.sort(sortFunc)
+  s.sort(sortFunc)
+  let i = 0
+  s.forEach(item => {
+    if (item >= g[i]) i++
+  })
+  return i
+};
