0638521fd78c58d4795ab71352b37829d825a41f
feedoom <qqdz3750@sina.com>
2020-09-25 00:11:32 +0800

    双指针遍历链表并删除

---
+++ 19.删除链表的倒数第n个节点.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=javascript
+ *
+ * [19] 删除链表的倒数第N个节点
+ *
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (39.47%)
+ * Likes:    995
+ * Dislikes: 0
+ * Total Accepted:    238.6K
+ * Total Submissions: 603.9K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ *
+ *
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let p1 = head;
+  let p2 = head;
+  for (let i = 0; i < n; i++) {
+    p2 = p2.next;
+  }
+  if (!p2) return head.next;
+  while (p2.next) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+  p1.next = p1.next.next;
+  return head;
+};
+// @lc code=end
