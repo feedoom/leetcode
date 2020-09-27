7b8f95cf3f251dd6b7e51d9d3cdb6897d8c38986
feedoom <qqdz3750@sina.com>
2020-09-27 21:15:13 +0800

    先序遍历

---
+++ 257.二叉树的所有路径.js
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=257 lang=javascript
+ *
+ * [257] 二叉树的所有路径
+ *
+ * https://leetcode-cn.com/problems/binary-tree-paths/description/
+ *
+ * algorithms
+ * Easy (66.01%)
+ * Likes:    371
+ * Dislikes: 0
+ * Total Accepted:    78.9K
+ * Total Submissions: 119.6K
+ * Testcase Example:  '[1,2,3,null,5]'
+ *
+ * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ *
+ * 输入:
+ *
+ * ⁠  1
+ * ⁠/   \
+ * 2     3
+ * ⁠\
+ * ⁠ 5
+ *
+ * 输出: ["1->2->5", "1->3"]
+ *
+ * 解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function (root) {
+  if (!root) return [];
+  let res = [];
+  const dfs = (root, str) => {
+    str += root.val + "->";
+    if (root.left) dfs(root.left, str);
+    if (root.right) dfs(root.right, str);
+    if (!root.left && !root.right) res.push(str.slice(0, str.length - 2));
+  };
+  dfs(root, "");
+  return res;
+};
+// @lc code=end
