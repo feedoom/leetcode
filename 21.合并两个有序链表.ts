dd5ae26b4899b3887488a9593046208dfcc15afd
feedoom <qqdz3750@sina.com>
2020-10-27 15:56:42 +0800

    合并两个有序链表 javascript, typescript

---
+++ 21.合并两个有序链表.ts
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=typescript
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (64.35%)
+ * Likes:    1345
+ * Dislikes: 0
+ * Total Accepted:    397.3K
+ * Total Submissions: 615.2K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const res: ListNode | null = new ListNode(0)
+    let p = res
+    let p1 = l1
+    let p2 = l2
+    while(p1 && p2) {
+        if(p1.val > p2.val) {
+            p.next = p2
+            p2 = p2.next
+        } else {
+            p.next = p1
+            p1 = p1.next
+        }
+        p = p.next
+    }
+    if (p1) p.next = p1
+    if (p2) p.next = p2
+
+    return res.next
+};
+// @lc code=end
+
