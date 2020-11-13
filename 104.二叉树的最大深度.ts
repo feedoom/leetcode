174894edd793cd06ee598be19268266a8b305679
feedoom <qqdz3750@sina.com>
2020-11-13 10:53:03 +0800

    二叉树的最大深度 ---> typescript

---
+++ 104.二叉树的最大深度.ts
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=typescript
+ *
+ * [104] 二叉树的最大深度
+ *
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (75.04%)
+ * Likes:    734
+ * Dislikes: 0
+ * Total Accepted:    302.8K
+ * Total Submissions: 402.1K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * 返回它的最大深度 3 。
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    let deep: number = 0;
+    const dfs = (root: TreeNode | null, currentDeep: number): void => {
+        if(!root) return ;
+        deep = Math.max(deep, currentDeep)
+        if(root.left) dfs(root.left, currentDeep + 1)
+        if(root.right) dfs(root.right, currentDeep + 1)
+    }
+    dfs(root, 1)
+    return deep
+};
+// @lc code=end
+
