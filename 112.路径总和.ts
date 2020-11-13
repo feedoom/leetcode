b8cc71f819e4dcfbc798ec12eaeddcc69acffd12
feedoom <qqdz3750@sina.com>
2020-11-13 11:27:35 +0800

    路径总和 ---> typescript

---
+++ 112.路径总和.ts
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=typescript
+ *
+ * [112] 路径总和
+ *
+ * https://leetcode-cn.com/problems/path-sum/description/
+ *
+ * algorithms
+ * Easy (51.13%)
+ * Likes:    461
+ * Dislikes: 0
+ * Total Accepted:    150K
+ * Total Submissions: 291.9K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,null,1]\n22'
+ *
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例: 
+ * 给定如下二叉树，以及目标和 sum = 22，
+ *
+ * ⁠             5
+ * ⁠            / \
+ * ⁠           4   8
+ * ⁠          /   / \
+ * ⁠         11  13  4
+ * ⁠        /  \      \
+ * ⁠       7    2      1
+ *
+ *
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, sum: number): boolean {
+    if(!root) return false;
+    function dfs(root: TreeNode | null, currentSum: number) {
+        if(!root) return false;
+        if(!root.left && !root.right && currentSum === sum) res = true;
+        if(root.left) dfs(root.left, currentSum + root.left.val)
+        if(root.right) dfs(root.right, currentSum + root.right.val)
+    }
+    let res: boolean = false
+    dfs(root!, root!.val)
+    return res
+};
+// @lc code=end
+
