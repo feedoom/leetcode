71aabc57a1ac57942f7dadf1261441121ab04d6c
feedoom <qqdz3750@sina.com>
2020-11-14 22:35:50 +0800

    搜索插入位置 ---> typescript

---
+++ 35.搜索插入位置.ts
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=typescript
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (46.68%)
+ * Likes:    737
+ * Dislikes: 0
+ * Total Accepted:    275.7K
+ * Total Submissions: 589.3K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ *
+ *
+ */
+
+// @lc code=start
+function searchInsert(nums: number[], target: number): number {
+    let left: number = 0;
+    let right: number = nums.length - 1;
+    let mid: number = (left + right) >> 1;
+    while(left <= right) {
+        mid = (left + right) >> 1;
+        if(nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return left;
+};
+// @lc code=end
+
